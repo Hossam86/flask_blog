f01d398ba370bae80c0fba207656b7e09dcff95a
Hossam Samir <hossamsamir@outlook.com>
2022-02-26 17:09:12 +0200

    formating

--- blog_app.py
+++ blog_app.py
@@ -3,50 +3,52 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField ,SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 # Initlization application instance
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-# Routes and viewfunctions
 
-# config
-app.config['SECRET_KEY']='bd778a7ba38c685220464a94'
+# Routes and viewfunctions
+# Configuration
+app.config['SECRET_KEY'] = 'bd778a7ba38c685220464a94'
 
 # Forms
 class NameForm(FlaskForm):
-    name= StringField(label="Name",validators=[DataRequired(),])
-    submit=SubmitField(label="Submit")
+    name = StringField(label="Name", validators=[DataRequired(), ])
+    submit = SubmitField(label="Submit")
 
-# application root url
-@app.route('/',methods=['POST', 'GET'])
+# Application root url
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    name=None
-    form=NameForm()
+    name = None
+    form = NameForm()
     if form.validate_on_submit():
-        name=form.name.data
-        form.name.data=''
+        name = form.name.data
+        form.name.data = ''
     return render_template("index.html", current_time=datetime.utcnow(), form=form, name=name)
 
-# dynamics routing
+
+# Dynamics routing
 @app.route('/user/<name>')
 def profile_page(name):
     return render_template('user.html', name=name)
 
-# the request- repoponse cycle
+# request- repoponse cycle
 # application and request context
-  
 @app.route('/client')
 def client_agent():
-    user_agent= request.headers["User-Agent"]
-    response=make_response("this document carries cookie")
-    response.set_cookie('answer','42')
+    user_agent = request.headers["User-Agent"]
+    response = make_response("this document carries cookie")
+    response.set_cookie('answer', '42')
     return response
 
+
 @app.errorhandler(404)
-def page_not_found (e):
+def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
